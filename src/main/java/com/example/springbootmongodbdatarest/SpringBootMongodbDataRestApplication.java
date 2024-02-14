7f0d822d20cedb120554998b6f95a7cb18bca080
KelvinCY <kelvincyhk@gmail.com>
2024-02-14 01:43:18 -0800

    First commit

---
+++ src/main/java/com/example/springbootmongodbdatarest/SpringBootMongodbDataRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootmongodbdatarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMongodbDataRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMongodbDataRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootmongodbdatarest/SpringBootMongodbDataRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootmongodbdatarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMongodbDataRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
